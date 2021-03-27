4b61b846be052f1d22114658242c461bff4e35ca
LukeG007 <69555173+LukeG007@users.noreply.github.com>
2021-03-26 22:21:11 -0400

    Update popups.py

--- PC Destroyer/popups.py
+++ PC Destroyer/popups.py
@@ -5,7 +5,7 @@ root = Tk()
 root.geometry('0x0')
 choice = random.randrange(1, 4)
 if choice == 1:
-    tkinter.messagebox.showinfo('1 new message from Lucas Gilsinger', 'Hey babe ;)')
+    tkinter.messagebox.showinfo('1 new message from Gordon Ramsey', 'Hey babe ;)')
 if choice == 2:
     tkinter.messagebox.showerror('1 new message from ur mum', 'Do ur homework')
 if choice == 3:
